e36ce4e4b7b74a4ef7218657e3ffc9f174efa8ba
erinworth <eeworth@gmail.com>
2017-11-28 18:04:23 -0800

    Commit Again

--- lib/kele.rb
+++ lib/kele.rb
@@ -15,7 +15,7 @@ class Kele
   end
 
   def get_me
-    response = self.class.get(url, headers: { "authorization" => @auth_token })
+    response = self.class.get("/users/me", headers: { "authorization": @auth_token })
     JSON.parse(response.body)
   end
 end
