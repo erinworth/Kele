ebae08f7f2f06e0e9ea84bdef7d7e78f918a0b73
erinworth <eeworth@gmail.com>
2017-12-06 13:34:24 -0800

    part 2 done

--- lib/kele.rb
+++ lib/kele.rb
@@ -29,4 +29,9 @@ class Kele
     JSON.parse(response.body)
   end
 
+  def get_checkpoint(checkpoint_id)
+    response = self.class.get("/checkpoints/#{checkpoint_id}", headers: { "authorization" => @auth_token })
+    JSON.parse(response.body)
+  end
+
 end
